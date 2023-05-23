e9fb0c3d03c6b7d20bd7d0690789bea8883dc8cc
maelaubert <maelaubert56@gmail.com>
2023-05-23 02:29:28 +0200

    commit

---
+++ js/header_load.js
@@ -0,0 +1,26 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    load_header();
+})
+
+window.addEventListener("resize", function(){
+    load_header();
+
+    /* if the text is too long, hide the top header */
+    /* we show the top header to be able to calculate the width */
+    document.querySelector(".top_header").style.display = "flex";
+    if (document.querySelector(".top_header_text").offsetWidth +20 > document.querySelector(".top_header").offsetWidth){
+        document.querySelector(".top_header").style.display = "none";
+    }
+})
+
+
+function load_header(){
+    /* set the height of the toggle icon */
+    var top_header_height = document.querySelector(".top_header").offsetHeight;
+    var bottom_header_height = document.querySelector(".bottom_header").offsetHeight;
+    var sidebarIconToggle_height = document.querySelector(".sidebarIconToggle").offsetHeight;
+    document.querySelector(".sidebarIconToggle").style.top = (top_header_height + bottom_header_height/2) - sidebarIconToggle_height/2 + "px";
+}
+
+
