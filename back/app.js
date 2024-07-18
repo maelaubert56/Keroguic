c72c79268eea31cc31edd63b6f30e3d535dfe2c0
Maël Aubert <maelaubert56@gmail.com>
2024-07-18 04:39:30 +0200

    changes

--- back/app.js
+++ back/app.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json());
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
@@ -36,7 +36,7 @@ app.use("/gallery", require("./routes/gallery"));
 app.use("/data", require("./routes/data"));
 
 app.listen(process.env.PORT, () => {
-  console.log("Server is running at http://localhost:3000");
+  console.log("Server is running at http://localhost:" + process.env.PORT);
 });
 
 module.exports = app;
